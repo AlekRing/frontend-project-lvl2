e848faf20f38dc44e77d05c08f476db314488651
Alek_ring <okolcov60@gmail.com>
2022-05-22 12:47:30 +0300

    gendiff: fix linter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@jest/globals';
 import gendiff from '../index.js';
-import {gendiffOutput, path1, path2} from '../__fixtures__/gendiff.fixture.js';
+import { gendiffOutput, path1, path2 } from '../__fixtures__/gendiff.fixture.js';
 
 test('gendiff', () => {
   expect(gendiff(path1, path2)).toBe(gendiffOutput);
